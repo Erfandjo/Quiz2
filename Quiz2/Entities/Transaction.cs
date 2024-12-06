540df107bc2c75d4f00bbc1580f93e7a5a95fdae
Erfandjo <erfan90johrzadh@gmail.com>
2024-12-06 09:12:52 +0330

    Add new Ability

--- Quiz2/Entities/Transaction.cs
+++ Quiz2/Entities/Transaction.cs
@@ -5,6 +5,7 @@
         public int TransactionId { get; set; }
         public string SourceCardNumber { get; set; }
         public string DestinationCardNumber { get; set; }
+        public DateTime ActionAt { get; set; }
         public float Amount { get; set; }
         public DateTime TransactionDate { get; set; }
         public bool isSuccessful { get; set; }
